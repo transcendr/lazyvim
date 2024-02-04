e089dd5735fd0a6fbc6e783d6beaefe1859d8630
Bryan Price-McConnahea <transcendrstudio@gmail.com>
2024-02-04 11:40:38 +0100

    Add keymap_utils.lua for managing key mappings in Lua configuration.

---
+++ lua/config/keymap_utils.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+local function bind(op, outer_opts)
+  outer_opts = vim.tbl_extend("force", { noremap = true, silent = true }, outer_opts or {})
+
+  return function(lhs, rhs, opts)
+    opts = vim.tbl_extend("force", outer_opts, opts or {})
+    vim.keymap.set(op, lhs, rhs, opts)
+  end
+end
+
+M.map = bind("")
+M.nmap = bind("n", { noremap = false })
+M.nnoremap = bind("n")
+M.vnoremap = bind("v")
+M.xnoremap = bind("x")
+M.inoremap = bind("i")
+--M.tnoremap = bind("t")
+--tnoremap except for lazygit windows
+M.tnoremap = function(lhs, rhs, opts)
+  if vim.bo.filetype == "lazygit" then
+    vim.api.nvim_buf_set_keymap(0, "t", lhs, rhs, opts)
+  else
+    vim.notify("tnoremap: " .. lhs .. " " .. rhs)
+    vim.api.nvim_set_keymap("t", lhs, rhs, opts)
+  end
+end
+
+return M
